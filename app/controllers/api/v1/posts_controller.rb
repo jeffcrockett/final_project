8707655b3021df8d8a7c99447cd71693449cd64d
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-10-14 19:24:44 -0400

    Can now create posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::PostsController < ApplicationController
     def show
         render json: Post.find(params[:id])
     end
+
+    def create 
+        render json: Post.create(post_params)
+    end
+
+    private 
+    def post_params
+        params.require(:post).permit(:subforum_id, :title, :content, :user_id)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_10_193533) do
+ActiveRecord::Schema.define(version: 2018_10_14_211338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_10_10_193533) do
     t.datetime "updated_at", null: false
     t.integer "upvotes"
     t.integer "downvotes"
+    t.string "content"
   end
 
   create_table "subforums", force: :cascade do |t|
