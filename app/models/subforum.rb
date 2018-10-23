704df5aaa88dbf67fb32578ccc391ced99ea876a
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-10-23 09:11:23 -0400

    Changed ordering of user posts and user comments

--- app/models/subforum.rb
+++ app/models/subforum.rb
@@ -1,6 +1,6 @@
 class Subforum < ApplicationRecord
     validates :name, uniqueness: true
-    has_many :posts
+    has_many :posts, -> { order('upvotes - downvotes DESC') }
     has_many :subscriptions
     has_many :users, through: :subscriptions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
-    has_many :comments, -> { order('id DESC') }
-    has_many :posts, -> { order('id DESC') }
+    has_many :comments, -> { order('upvotes - downvotes DESC') }
+    has_many :posts, -> { order('upvotes - downvotes DESC') }
     has_many :subscriptions
     has_many :subforums, through: :subscriptions
     # def ordered_comments
