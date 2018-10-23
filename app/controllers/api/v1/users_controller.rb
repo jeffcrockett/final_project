8f078e5c6aaed7a2f800ad6243ff2f2b6036be35
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-10-23 11:45:16 -0400

    Added navigation links to profile page

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,7 +20,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
     def show
-        render json: User.find(params[:id]), include: ['comments', 'posts', 'subscriptions','subforums.posts']
+        render json: User.find(params[:id]), include: ['comments.post', 'posts', 'subscriptions','subforums.posts']
     end
 
     private
